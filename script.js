5d2b0e50fdd666118d1b42627139338b685d19a4
selim <nfc.selim@gmail.com>
2023-06-17 21:16:51 +0200

    Bigger screen logic

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ setInterval(() => {
 
 
 
-}, 1);
+}, 100);
 
 function addZeros(number) {
     if (number < 10) {
@@ -86,3 +86,117 @@ function addZeros(number) {
     }
     return number
 }
+
+
+
+
+
+
+
+let expandButton = document.getElementById('expand');
+let musicBlock = document.getElementById('musicBlock');
+let playingMusicController = document.getElementById('playingMusicController');
+let musicVolumeBigger = document.getElementById('musicVolumeBigger');
+let playingMusicBar = document.getElementById('playingMusicBar');
+let playingMusicOverview = document.getElementById('playingMusicOverview');
+let CoverPictureBar = document.getElementById('CoverPictureBar');
+let musicNameBar = document.getElementById('musicNameBar');
+let artistNameBar = document.getElementById('artistNameBar');
+
+let isExpanded = false;
+
+expandButton.addEventListener("click", function() {
+  if (!isExpanded) {
+
+    expandButton.innerHTML = '<ion-icon name="close"></ion-icon>';
+
+    musicBlock.style.height = '100vh';
+    musicBlock.style.justifyContent = 'center';
+    musicBlock.style.flexDirection = 'column';
+
+    musicVolumeBigger.style.marginRight = '0';
+    musicVolumeBigger.style.right = '50px';
+    musicVolumeBigger.style.width = '100%';
+    musicVolumeBigger.style.justifyContent = 'flex-end';
+    musicVolumeBigger.style.bottom = '30px';
+    musicVolumeBigger.style.position = 'fixed';
+    musicVolumeBigger.style.zIndex = '-1';
+
+    playingMusicOverview.style.marginLeft = '0';
+    playingMusicOverview.style.position = 'fixed';
+    playingMusicOverview.style.bottom = '190px';
+    playingMusicOverview.style.left = '200px';
+
+    CoverPictureBar.style.width = '250px';
+    CoverPictureBar.style.height = '250px';
+
+    musicNameBar.style.fontSize = '60px';
+    artistNameBar.style.fontSize = '20px';
+    artistNameBar.style.marginLeft = '5px';
+
+    playingMusicController.style.display = 'flex';
+    playingMusicController.style.flexDirection = 'column-reverse';
+    playingMusicController.style.position = 'absolute';
+    playingMusicController.style.bottom = '50px';
+    playingMusicController.style.width = '100%';
+
+    playingMusicBar.style.justifyContent = 'center';
+
+    progress.style.width = '80%';
+    progress.style.justifyContent = 'center';
+
+    isExpanded = true;
+  } else {
+    musicBlock.style.height = '';
+    musicBlock.style.display = '';
+    musicBlock.style.justifyContent = '';
+    musicBlock.style.flexDirection = '';
+
+    musicVolumeBigger.style.marginRight = '';
+    musicVolumeBigger.style.right = '';
+    musicVolumeBigger.style.display = '';
+    musicVolumeBigger.style.width = '';
+    musicVolumeBigger.style.justifyContent = '';
+    musicVolumeBigger.style.bottom = '';
+    musicVolumeBigger.style.position = '';
+    musicVolumeBigger.style.zIndex = '';
+
+    playingMusicOverview.style.marginLeft = '';
+    playingMusicOverview.style.display = '';
+    playingMusicOverview.style.position = '';
+    playingMusicOverview.style.bottom = '';
+    playingMusicOverview.style.left = '';
+
+    CoverPictureBar.style.width = '';
+    CoverPictureBar.style.height = '';
+    CoverPictureBar.style.objectFit = '';
+    CoverPictureBar.style.padding = '';
+    CoverPictureBar.style.display = '';
+    CoverPictureBar.style.alignItems = '';
+
+    musicNameBar.style.fontSize = '';
+
+    artistNameBar.style.fontSize = '';
+    artistNameBar.style.fontWeight = '';
+    artistNameBar.style.marginLeft = '';
+
+    playingMusicController.style.display = '';
+    playingMusicController.style.flexDirection = '';
+    playingMusicController.style.position = '';
+    playingMusicController.style.bottom = '';
+    playingMusicController.style.width = '';
+
+    playingMusicBar.style.justifyContent = '';
+    playingMusicBar.style.alignItems = '';
+    playingMusicBar.style.display = '';
+
+    progress.style.width = '';
+    progress.style.justifyContent = '';
+
+    expandButton.innerHTML = '<ion-icon name="expand"></ion-icon>';
+
+    isExpanded = false;
+  }
+});
+
+
